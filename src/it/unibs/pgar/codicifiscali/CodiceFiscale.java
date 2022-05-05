ac9c48540416fb287f628594c98288235da0e2af
dvdpezzo <d.pezzotti005@studenti.unibs.it>
2022-05-05 15:32:44 +0200

    Aggiunta della struttura dei metodi in CodiceFiscale e alcuni commenti Aggiunta nella classe InterazioneXML del metodo leggiCodiceFiscale e inizio metodo scriviXML

--- src/it/unibs/pgar/codicifiscali/CodiceFiscale.java
+++ src/it/unibs/pgar/codicifiscali/CodiceFiscale.java
@@ -7,6 +7,25 @@ public class CodiceFiscale {
 
     ArrayList<Character> mesi = new ArrayList<Character>();
 
+    public boolean controlloCodice(String codice){
+        //aggiungere vari metodi substring che controllano i vari pezzi del codice che gli passiamo
+        //restituisce true se è corretto false se non lo è
+    }
+
+    public void aggiungiCodiceInvalido(String codiceDaAggiungere, String[] codiciInvalidi){
+        //a questo metodo passiamo un codice invalido(determinato dal metodo controllo codice) e lo aggiungiamo al vettore dei codici invalidi
+
+    }
+
+    public String generaCodice(Persona persona){
+        //questo metodo riceve in entrata i dati di una persona e fa le opportune trasformazioni per ottenere il codice fiscale
+
+    }
+
+    private void aggiungiCodice(String codiceDaAggiungere, String[] codici){
+        //questo metodo riceve in entrata un codice fiscale corretto e lo aggiunge al vettore dei codici fiscali corretti
+
+    }
 
 
 
--- src/it/unibs/pgar/codicifiscali/InterazioneXML.java
+++ src/it/unibs/pgar/codicifiscali/InterazioneXML.java
@@ -1,15 +1,22 @@
 package it.unibs.pgar.codicifiscali;
 
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
-import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.*;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 public class InterazioneXML {
+
+    public static final int N_COD_FISC_FILE = 1000;
+    public static final String READER_ERROR = "Errore nell'inizializzazione del reader:";
+    public static final boolean WRITER_ERROR = false;
+    public static final String UTF_8 = "utf-8";
+    public static final String VERSION = "1.0";
+    public static final int N_COMUNI_FILE = 8092;
+    public static final int INT = 2;
+
     //Matrice di Comuni
-    String[][] comuni = new String[8092][2];
+    String[][] comuni = new String[N_COMUNI_FILE][INT];
 
     ArrayList<Integer> Persone = new ArrayList<Integer>();
 
@@ -39,14 +46,14 @@ public class InterazioneXML {
                             xmlr.getAttributeValue(i));
                 //prendere atributi delle persone e spostarle nell'array di persone
                 break;
-            case XMLStreamConstants.CHARACTER:
+            case XMLStreamConstants.CHARACTERS:
 
                 break;
             case XMLStreamConstants.END_ELEMENT://Stampa fine elemento
                 System.out.println("End-Tag" + xmlr.getLocalName());
                 break;
         }
-        xmlr.next;
+        xmlr.next();
 // mi piace la pizza
 
     }
@@ -71,10 +78,10 @@ public class InterazioneXML {
                 break;
             case XMLStreamConstants.START_ELEMENT://Stampa inizio elemento
                 System.out.println("Tag" + xmlr.getLocalName());
-                for(int i = 0; i <xmlr.getAttributeCount(); i++);
-                System.out.println(" => attributo %s->%s%n", xmlr.getAttributeLocalName(i),
-                        xmlr.getAttributeValue(i));
-
+                for(int i = 0; i <xmlr.getAttributeCount(); i++) {
+                    System.out.println(" => attributo %s->%s%n", xmlr.getAttributeLocalName(i),
+                            xmlr.getAttributeValue(i));
+                }
                 break;
             case XMLStreamConstants.END_ELEMENT://Stampa fine elemento
                 System.out.println("End-Tag" + xmlr.getLocalName());
@@ -98,7 +105,56 @@ public class InterazioneXML {
                 }
 
     }*/
+    public String[] leggiCodiciFiscali(String fileName) throws XMLStreamException {
+        String[] vettoreCodiciFiscali = new String[N_COD_FISC_FILE];
+        int i = 0;
+        XMLInputFactory xmlif;
+        XMLStreamReader xmlr = null;
+        try {
+            xmlif = XMLInputFactory.newInstance();
+            xmlr = xmlif.createXMLStreamReader(fileName, new FileInputStream(fileName));
+        } catch (Exception e) {
+            System.out.println(READER_ERROR);
+            System.out.println(e.getMessage());
+
+        }
+        while (xmlr.hasNext()) {
+            switch (xmlr.getEventType()) {
+                case XMLStreamConstants.START_DOCUMENT: // inizio del documento: non fa nulla
+                    break;
+                case XMLStreamConstants.START_ELEMENT: // inizio di un elemento: non fa nulla
+                    break;
+                case XMLStreamConstants.END_ELEMENT: // fine di un elemento: ogni volta che trova un fine tag aumenta i di 1
+                    i++;
+                    break;
+                case XMLStreamConstants.COMMENT:
+                    break; // commento: non fa nulla
+                case XMLStreamConstants.CHARACTERS: // content all’interno di un elemento: lo aggiunge al vettoreCodiciFiscali
+                    if (xmlr.getText().trim().length() > 0){ // controlla se il testo non contiene solo spazi
+                        vettoreCodiciFiscali[i] = xmlr.getText();  //il testo trovato lo aggiunge al vettore al posto i
+                    }
+                    break;
+            }
+            xmlr.next();
+        }
+        return vettoreCodiciFiscali; //restituisce il vettore contenente tutti i codici fiscali da controllare
+    }
+    //Davide: non avevo visto che avevi già fatto tu il metodo leggicodicifiscali, sorry!
+
+    public void scriviXML(String filename, Persona persona){
+        XMLOutputFactory xmlof = null;
+        XMLStreamWriter xmlw = null;
+        try {
+            xmlof = XMLOutputFactory.newInstance();
+            xmlw = xmlof.createXMLStreamWriter(new FileOutputStream(filename), UTF_8);
+            xmlw.writeStartDocument(UTF_8, VERSION);
+        } catch (Exception e) {
+            System.out.println(WRITER_ERROR);
+            System.out.println(e.getMessage());
+        }
 
 
 
-}
+
+
+    }
